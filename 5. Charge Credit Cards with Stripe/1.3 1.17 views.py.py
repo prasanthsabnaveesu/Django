3c81600942c0178ad9ba6d383b34116baad2a0e6
prasanthsabnaveesu <46451701+prasanthsabnaveesu@users.noreply.github.com>
2022-03-28 22:14:46 +0530

    Add files via upload

---
+++ 5. Charge Credit Cards with Stripe/1.3 1.17 views.py.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+# Create your views here.
+
+@login_required
+def checkout(request):
+	publishKey = settings.STRIPE_PUBLISHABLE_KEY
+	if request.method == 'POST':
+		token = request.POST['stripeToken']
+		# Create a charge: this will charge the user's card
+		try:
+		  charge = stripe.Charge.create(
+		      amount=1000, # Amount in cents
+		      currency="usd",
+		      source=token,
+		      description="Example charge"
+		  )
+		except stripe.error.CardError as e:
+		  # The card has been declined
+		  pass
+	context = {'publishKey': publishKey}
+	template = 'checkout.html'
+	return render(request,template,context)
